fef2cc514f274d5a3156fa5bc085bb85d0f21483
Felipe <felipelimamorais@gmail.com>
2015-11-17 18:02:42 -0300

    insersão do core

---
+++ core.py
@@ -0,0 +1,120 @@
+"""
+Core Algorithm
+"""
+
+import conf, util, random, math
+import schwefel
+import rastrigin
+
+def run(problem):
+
+    ### MASTER ###
+
+    pop = util.new_population(problem)
+    population_size = len(pop)
+
+    models = []
+    last_generated = pop[0]
+
+    med = 1
+    models.append(util.create_model(last_generated, med))
+    for x in xrange(1, population_size):
+        if util.similarity_of_individuals(pop[x], last_generated) <= 0.25:
+            models.append(util.create_model(pop[x], med))
+            last_generated = pop[x]
+
+    aux = []
+    for x in models:
+        aux.append(list(x))
+
+    #### SLAVE ###
+    print population_size
+    print len(models)
+
+    print '[',
+    for x in models[0]:
+        print '%.2f' % x,
+    print ']'
+    # Generating the new population for each probabilist model #
+    oi = 1
+    last = problem.get_fitness(last_generated)
+    while True:
+    #while oi < 1000:
+        print '------init------'
+       
+        #TODO
+        populations = []
+        for model_id in xrange(len(models)):
+            populations.append(util.new_population(problem))
+
+        # The magic #
+        for model_id in xrange(len(models)):
+            for model_item_id in xrange(len(models[model_id])):
+                loop = False
+                while not loop:
+                    for bit_id in xrange(len(populations[model_id][model_item_id])):
+                        if random.random() < models[model_id][model_item_id]:
+                            populations[model_id][model_item_id][bit_id] = '1'
+                        else:
+                            populations[model_id][model_item_id][bit_id] = '0'
+                    loop = problem.validate_individual(populations[model_id][model_item_id])
+
+
+        # Calculate the best individual of each probabilist model #
+        bests = [None] * len(models)
+        for model_id in xrange(len(models)):
+            bests[model_id] = populations[model_id][0]
+            for model_item_id in xrange(1, len(models[model_id])):
+                if math.fabs(problem.get_fitness(bests[model_id])) > math.fabs(problem.get_fitness(populations[model_id][model_item_id])):
+                    bests[model_id] = populations[model_id][model_item_id]
+
+        # Calculate the bestest #
+        b = bests[0]
+
+        for best in bests:
+            if problem.get_fitness(b) > problem.get_fitness(best):
+                b = best
+                print "id best:  ", bests.index(best)
+        print "melhor atual:", b
+
+        # B is the best #
+        if problem.is_finished(b):
+            print 'FINISHED'
+            print problem.get_fitness(b)
+            print problem.show(b)
+            return
+
+        # Calculate the new probabilist models #
+        for model in models:
+            util.update_model(model, b)
+
+        # Mutate each probabilist model #
+        for model in models:
+            util.mutate_model(model)
+
+        # Learing model
+        new = math.fabs(problem.get_fitness(b))
+        util.learning(last, new)
+        last = new
+
+        print '[',
+        for x in models[0]:
+            print '%.2f' % x,
+        print ']'
+
+        print "coordenadas: ", problem.show(b)
+        print b
+        print "melhor fitness: ", problem.get_fitness(b)," Alfa: ", conf.ALPHA
+        print '------end------'
+        oi += 1
+
+
+    for x in xrange(len(models)):
+        print '>>', x, '<<'
+        for y in xrange(len(models[x])):
+            print aux[x][y], " -> ", models[x][y]
+
+if __name__ == '__main__':
+    print 'INIT'
+    run(rastrigin)
+    print 'END'
